ab1d6596a2f5c90494eac2aabd1a97c4cfcac49c
DipeshKazi <dipesh2025@hotmail.com>
2017-05-10 10:28:48 -0500

    Update

--- C++Final/C++Final/Source.cpp
+++ C++Final/C++Final/Source.cpp
@@ -4,10 +4,10 @@
 int main() {
 
 
-	std::string Yes_No, anwser;
+	std::string Yes_No, anwser, name;
 	int tries = 5;
 	char difficulty = ' ';
-
+	
 
 
 	std::cout << "Welcome TO:  " << std::endl;
@@ -24,14 +24,59 @@ int main() {
 	{
 		std::cout << " " << std::endl;
 		std::cout << "Thank you for playing." << std::endl;
-		std::cout << "Please chose a difficulty: Easy, Medium, Hard." << std::endl;
+		std::cout << "Please chose a difficulty: Easy = E, Medium = M, Hard = H." << std::endl;
 		std::cin >> difficulty;
 		difficulty = toupper(difficulty);
-
+		
 		switch (difficulty)
 		{
 
-			case ''
+			//case ''
+
+
+				
+
+			case 'M':
+				std::cout << " " << std::endl;
+				std::cout << "In this level you have 5 lives to get all 8 question right." << std::endl;
+				std::cout << "       " << std::endl;
+				std::cout << " " << std::endl;
+				std::cout << " " << std::endl;
+				std::cout << " " << std::endl;
+			                   
+
+				std::cout << " # 1) Name the world's biggest island" << std::endl;
+				std::cout << " A) Madagascar" << std::endl;
+				std::cout << " B) Greenland " << std::endl;
+				std::cout << " C) United Kingdom" << std::endl;
+				std::cout << " D) Ireland" << std::endl;
+				std::cin >> name;
+				if (name == "B")
+				{
+					std::cout << "Good JOb" << std::endl;
+				}
+
+				else if (name == "D")
+				{
+					std::cout << "Wrong answer" <<  std::endl;
+					tries--;
+				}
+				else if (name == "A")
+				{
+					std::cout << "Wrong answer" << std::endl;
+					tries--;
+				}
+
+				else if (name == "C")
+				{
+					std::cout << "Wrong answer" << std::endl;
+					tries--;
+				}
+				std::cout << " You have " << tries  << " lives  left  " << std::endl;
+
+				std::cout << " # 2) Name the world's biggest island" << std::endl;
+				break;
+
 
 
 		}
